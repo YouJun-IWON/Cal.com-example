e899e093f45958018c7b29017e9d940c14c6f670
ReasonJun <117330708+YouJun-IWON@users.noreply.github.com>
2024-10-28 15:05:30 +0900

    fix: :bug: set error

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import { Login } from "@/components/login";
+import { Metadata } from "next";
+
+
+
+export default function LoginPage() {
+  return (
+    <main className="">
+      <Login />
+    </main>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { CTA } from "@/components/cta";
+import { Hero } from "@/components/hero";
+
+
+
+export default function Home() {
+  return (
+    <main className="">
+      <Hero />
+  
+      <CTA />
+    </main>
+  );
+}
